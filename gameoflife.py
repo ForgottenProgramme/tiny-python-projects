48f1274d0f4434e2a82b8dd19b7c5e11429475db
Mahe Iram Khan <forgotten.programme@gmail.com>
2023-01-03 23:41:09 +0100

    add game of life

---
+++ gameoflife.py
@@ -0,0 +1,19 @@
+# Conway's Game of Life
+
+import random, time, copy
+
+WIDTH = 60
+HEIGHT = 20
+
+# Create a list of list for cells:
+nextCells = []
+for x in range(WIDTH):
+    column = [] # create new column
+    for y in range(HEIGHT):
+        if random.randint(0,1)==0:
+            column.append('#') # Add a living cell
+        else:
+            column.append('') # Add a dead cell.
+        
+    nextCells.append(column) # nextCells is a list of column lists.
+
